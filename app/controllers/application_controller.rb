520a4699d3d160036708a58a27cd9ce6e5d7e6e6
gogojojo <joseph.gochua@gmail.com>
2015-04-08 15:36:34 -0700

    added raiden character

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,6 @@ class ApplicationController < ActionController::Base
   end
   def miketyson
   end
+  def raiden
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'application#index'
   get '/wolverine' => 'application#wolverine' 
   get '/miketyson' => 'application#miketyson'
+  get '/raiden' => 'application#raiden'
 end
