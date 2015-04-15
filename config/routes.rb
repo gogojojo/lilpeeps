14a4d0720d9db6d668535a8fd3fe49b2a00e176c
gogojojo <joseph.gochua@gmail.com>
2015-04-15 16:53:25 -0700

    finished flash

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/humantorch' => 'application#humantorch'
   get '/spawn' => 'application#spawn'
   get '/drogo' => 'application#drogo'
+  get '/flash' => 'application#flash'
 end
